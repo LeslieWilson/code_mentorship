71553dd1dd4d67e732f462b62788a1b373e5dc37
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-12-03 18:25:17 -0500

    successful answer for part one

--- advent_of_code_day1/ruby.rb
+++ advent_of_code_day1/ruby.rb
@@ -1,1 +1,25 @@
-"test"
+# "test"
+
+
+# for every number, divide by 3. round down and subtract  2 to get the fuel requirement and then add all the fuel requiremnets
+
+# 1. I want something to read input.txt line by line
+
+  # File.foreach("input.txt") { |line| puts line }
+
+# 2. I want something that converts it to an integer, rounds it down and divides each line by 3
+
+# File.foreach("input.txt") { |line| puts (line.to_i)/3}
+#
+# 3. want something that subtracts 2
+
+# File.foreach("input.txt") { |line| puts ((line.to_i)/3)-2}
+
+# 4. want something that adds them all together
+
+sum = 0
+File.foreach("input.txt") { |line|
+  mass = ((line.to_i)/3)-2
+  sum +=mass
+}
+  puts sum
