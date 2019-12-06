c2d0d46bda05775cb4ef46a3cd85f2362df678ab
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-12-06 07:17:25 -0500

    quiz for challenge

---
+++ job_coding_challenges/rithm/quiz.js
@@ -0,0 +1,40 @@
+
+let color ='red'
+switch(color){
+  case "yellow":
+  console.log ("yellow");
+  break;
+  case "red":
+  console.log("red!!!");
+  break;
+  case "purple":
+  console.log("purple");
+  break;
+  case "orange":
+  console.log("orange")
+  break;
+  case "black":
+  console.log("black")
+  break;
+  default:"thats not a color"
+}
+
+var name = 'susan'
+name ? console.log("susan!!!"):console.log("other");
+
+var num = 3
+
+if (num > 5){
+  console.log("yeah its grter")
+}else{
+  console.log("nope")
+}
+
+
+num = 15
+
+if(num > 10 && num < 20 && num != 15 || num > 100){
+  console.log('yeahh')
+}else {
+    console.log('no')
+  }
