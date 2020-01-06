2803460ee5084ee730c9bfafed720f53ccd076c2
LeslieWilson <leslie.wilson.vt@gmail.com>
2020-01-06 15:30:50 -0500

    react practice and algorithm

---
+++ other/nested_json/file.js
@@ -0,0 +1,95 @@
+// directions: write  function  that will, if you give it a key, return all the values of that key stored in  an array
+
+data = [
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0002",
+		"type": "donut",
+		"name": "Raised",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0003",
+		"type": "donut",
+		"name": "Old Fashioned",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
+]
+
+
+function recursive_search(data, desired_key, hits=[]){
+  if (type(data) == "Array"){
+    data.forEach(function(item){
+      recursive_search(item, desired_key, hits)
+  }
+  })
+
+}
+}
+
+
+// function findValues(data){
+//   data.forEach(function(dictionary){
+//     Object.keys(dictionary).forEach(function(key){
+//       console.log(key)
+//     })
+//
+//   })
+// }
+//
+// findValues(data)
--- react_practice/src/containers/MyName.js
+++ react_practice/src/containers/MyName.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Tile1 from '../components/Tile1.js'
+import ImageComponent from '../components/ImageComponent.js'
 
 
 const MyName = (props) =>{
@@ -20,12 +21,34 @@ const MyName = (props) =>{
     )
   })
 
+  let image = [
+    {image:"https://i.postimg.cc/28ntcKB0/Screen-Shot-2020-01-03-at-3-26-54-PM.png",
+    label:"pic 1ljkhlfkjdshflksajdfh"},
+    {image:"https://i.postimg.cc/28ntcKB0/Screen-Shot-2020-01-03-at-3-26-54-PM.png",
+   label: "pic2alskdjfhlksadjfhlakjdfh"}
+  ]
+
+  let imagepic = image.map(pic =>{
+    return(
+      <ImageComponent
+      image = {pic.image}
+      label = {pic.label}
+      />
+    )
+  })
+
   return(
     <div>
     <h1>Hi Im Leslie</h1>
     {information}
+    {imagepic}
     </div>
   )
+
+
+
 }
+
+
 
 export default MyName
