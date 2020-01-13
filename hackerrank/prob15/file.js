8b7bf831218625e2b6d4f1bff20612d7957afcbc
LeslieWilson <leslie.wilson.vt@gmail.com>
2020-01-12 19:36:58 -0500

    started prob15

---
+++ hackerrank/prob15/file.js
@@ -0,0 +1,11 @@
+// DIRECTIONS: Sam's house has an apple tree and an orange tree that yield an abundance of fruit. In the diagram below, the red region denotes his house, where s is the start point, and t is the endpoint. The apple tree is to the left of his house, and the orange tree is to its right. You can assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b.
+//
+// determine how many apples and oranges will fall on sams house
+//
+// FOR FULL DIRECTIONS SEE LINK: https://www.hackerrank.com/challenges/apple-and-orange/problem?fbclid=IwAR1DJEFInt2Dwy2rBrmCeNsX92kVwCocwUITyblF77lVReagqUa50vDAWoI
+
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+
+
+}
