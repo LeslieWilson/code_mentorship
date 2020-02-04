26b671f9940a94fd0bc10326a544c011ffbcc686
LeslieWilson <leslie.wilson.vt@gmail.com>
2020-02-04 08:28:08 -0500

    utopian tree problem finished

--- hackerrank/utopian_tree/file.js
+++ hackerrank/utopian_tree/file.js
@@ -1,22 +1,24 @@
-// DIRECTIONS:The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
-//
-// Laura plants a Utopian Tree sapling with a height of 1 meter at the onset of spring. How tall will her tree be after N growth cycles?
+// DIRECTIONS:The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter. Laura plants a Utopian Tree sapling with a height of 1 meter at the onset of spring. How tall will her tree be after N growth cycles?
 
 // LINK: https://www.hackerrank.com/challenges/utopian-tree/problem?fbclid=IwAR0UO7ICX5PBX6vYUYJYfJbnVshTpT5DqJPFnmbfA2hOTVlX-iTFxzSd-Uw
 
 
-// go through n
-//
-// on the even number it doubles on the odd nuumber you add one
 
 function utopianTree(n){
-array = [...new Array(n)]
-   console.log(Object.getOwnPropertyNames(array))
-
+  let  sum = 0
+  for (let i = 0; i <= n + 1; i++){
+    if (i % 2 == 0){
+      sum = sum*2
+    }else if(i % 2 != 0){
+      sum += 1
+    }else if(i == 0){
+      sum += 1
+    }
+  }
+return sum
 }
 
 
 
-
-let n = [5]
+let n = 5
 console.log(utopianTree(n))
